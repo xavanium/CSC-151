45c40b57cce38421f5b3ed11546b7c08c4426f40
Xavanium <xavanium@gmail.com>
2026-02-05 18:59:09 +0000

    REAL dice added

--- demo.java
+++ demo.java
@@ -1,9 +1,12 @@
 import java.util.Scanner;
+import java.util.Random;
 public class demo {
 
     public static int roll() {
         //roll a die, return from 1 to 6
-        return 1; //for now just pretend
+        Random roller = new Random();
+        int roll = roller.nextInt(5) + 1; //0-5 becomes 1-6
+        return roll;
     }
     public static void main(String[] args) {
         Scanner k = new Scanner(System.in);
@@ -14,8 +17,12 @@ public class demo {
         int chips = 100;
         System.out.println("Well, " + name + ", you start with " + chips + " chips.");
         System.out.println();
+        //loop
+        for (int i=0; i<20; i++){
         System.out.println("Rolling the dice...");
-        int roll = roll();
-        System.out.println("You rolled a: "+roll);
+        int roll = roll() + roll();
+        System.out.println("You rolled a: "+roll); }
+        //end of program, closing scanner
+        k.close();
     }
 }
