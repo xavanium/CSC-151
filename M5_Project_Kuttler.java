c3919234235e08cd1ff42f07927c2db6ea7548db
Xavanium <xavanium@gmail.com>
2026-03-17 18:02:32 +0000

    started m5 project

---
+++ M5_Project_Kuttler.java
@@ -0,0 +1,9 @@
+public class M5_Project_Kuttler {
+    public static void main(String[] args) {
+        System.out.println("Jim inherited his business from his uncle.");
+        System.out.println("He builds metal warehouses.");
+        System.out.println("However, he needs to build his foundation on concrete, and he doesn't know a thing about concrete!");
+        System.out.println("So, let's help Jim out.");
+        
+    }
+}
